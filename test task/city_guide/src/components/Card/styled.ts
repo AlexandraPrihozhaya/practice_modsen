16f35ed03e03dcc91f35732cf129f1cd626f0bd4
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:41:47 +0300

    Update styled.ts

--- test task/city_guide/src/components/Card/styled.ts
+++ test task/city_guide/src/components/Card/styled.ts
@@ -1,59 +1,68 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { CardMedia, IconButton } from '@mui/material';
 
 export const SCard = styled.div`
-    border: 3px solid #C4C4C4;
-    border-radius: 10px;
-    width: 264px;
-    margin-bottom: 20px;
+  border: 3px solid #c4c4c4;
+  border-radius: 10px;
+  width: 264px;
+  margin-bottom: 20px;
 `;
 
 export const SDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const SCardMedia = styled(CardMedia)`
-    border-radius: 10px;
-    height: 80px;
-    max-width: 100px;
-    margin-right: 10px;
+  border-radius: 10px;
+  height: 80px;
+  max-width: 100px;
+  margin-right: 10px;
 `;
 
 export const SCardHeader = styled.p`
-    width: 120px;
-    font-size: 12px;
-    font-weight: 500;
+  width: 120px;
+  font-size: 12px;
+  font-weight: 500;
 `;
 
 export const SText = styled.p`
-    font-size: 10px;
-    justify-content: space-between;
-    display: -webkit-box;
-    -webkit-line-clamp: 3; 
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  font-size: 10px;
+  justify-content: space-between;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
-export const SIconButtonFav = styled(IconButton)`
-    svg {
-        color: #C75E5E;
-    }
+export const SIconButtonFav = styled.button`
+  background: none;
+  color: #c75e5e;
+  cursor: pointer;
+  font-size: 20px;
+  border: none;
 `;
 
-export const SIconButtonArrow = styled(IconButton)`
-    svg {
-        color: #373737;
-    }
+export const SIconButtonArrow = styled.button`
+  background: none;
+  color: #373737;
+  cursor: pointer;
+  font-size: 20px;
+  border: none;
 `;
 
 export const SCardActions = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const SCardContent = styled.div`
-    padding: 12px 12px 0 12px;
+  padding: 12px 12px 0 12px;
+`;
+
+export const SName = styled.text`
+  font-weight: 500;
+  font-size: 10px;
 `;
