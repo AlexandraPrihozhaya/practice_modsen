7c0fa20576c2d91dd8639753f99f863ac4a460d1
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:46:01 +0300

    Update styled.ts

--- test task/city_guide/src/components/InfoCard/styled.ts
+++ test task/city_guide/src/components/InfoCard/styled.ts
@@ -1,66 +1,66 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SButtonFav = styled.button`
-    padding: 7px 14px;
-    font-size: 14px;
-    background: #C75E5E;
-    border-radius: 5px;
-    border: none;
-    cursor: pointer;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-    width: 100%;
+  padding: 7px 14px;
+  font-size: 14px;
+  background: #c75e5e;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  width: 100%;
 `;
 
 export const SButtonRoute = styled.button`
-    padding: 7px 14px;
-    font-size: 14px;
-    background: #5E7BC7;
-    border-radius: 5px;
-    border: none;
-    cursor: pointer;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-    width: 100%;
+  padding: 7px 14px;
+  font-size: 14px;
+  background: #5e7bc7;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  width: 100%;
 `;
 
 export const SCard = styled.div`
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    width: 300px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  width: 300px;
 `;
 
 export const STitle = styled.text`
-    font-weight: 700;
-    font-size: 20px;
-    color: #405F7B;
+  font-weight: 700;
+  font-size: 20px;
+  color: #405f7b;
 `;
 
 export const SText = styled.p`
-    font-weight: 300;
-    font-size: 14px;
+  font-weight: 300;
+  font-size: 14px;
 `;
 
 export const SAddress = styled.a`
-    font-weight: 300;
-    font-size: 14px;
-    display: block; 
-    wordBreak: break-all;
-    textOverflow: ellipsis;
-    whiteSpace: nowrap; 
-    overflow: hidden;
+  font-weight: 300;
+  font-size: 14px;
+  display: block;
+  wordbreak: break-all;
+  textoverflow: ellipsis;
+  whitespace: nowrap;
+  overflow: hidden;
 `;
 
 export const SName = styled.text`
-    font-weight: 500;
-    font-size: 14px;
+  font-weight: 500;
+  font-size: 14px;
 `;
