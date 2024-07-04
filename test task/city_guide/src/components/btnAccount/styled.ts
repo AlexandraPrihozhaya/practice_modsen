980015fa878b1ae6730758f6340f9ac429b7b801
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:50:29 +0300

    Update styled.ts

--- test task/city_guide/src/components/btnAccount/styled.ts
+++ test task/city_guide/src/components/btnAccount/styled.ts
@@ -1,15 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SAvatar = styled.div`
-    position: absolute;
-    display: inline-block;
-    bottom: 45px;
-    left: 50%;
-    transform: translateX(-50%);
+  position: absolute;
+  display: inline-block;
+  bottom: 45px;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 export const SButton = styled.button`
-    background: none;
-    border: none;
-    cursor: pointer;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+`;
+
+export const SAvatarMobile = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
 `;
