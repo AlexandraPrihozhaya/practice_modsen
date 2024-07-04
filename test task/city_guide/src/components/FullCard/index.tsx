cf780eec1e3371f54945221649b8bfeddc11e74a
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:43:25 +0300

    Update index.tsx

--- test task/city_guide/src/components/FullCard/index.tsx
+++ test task/city_guide/src/components/FullCard/index.tsx
@@ -1,34 +1,92 @@
 import React from 'react';
 import {
-    SCard, SCardMedia, SCardHeader, SCardContent, SText, 
-    SCardActions, SButtonFav, SButtonGeo
-} from "./styled";
-import { IoMdBookmark } from "react-icons/io";
-import { FaLocationDot } from "react-icons/fa6";
+  SCard,
+  SCardMedia,
+  SCardHeader,
+  SCardContent,
+  SText,
+  SCardActions,
+  SButtonFav,
+  SButtonGeo,
+  STitle,
+  SBlock,
+  SIconButtonArrow,
+  SName,
+  SAddress,
+} from './styled';
+import { IoMdBookmark, IoMdArrowDropleft } from 'react-icons/io';
+import { FaLocationDot } from 'react-icons/fa6';
+import { setShow } from '../../store/reducers/geoObjects';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 
 const FullCard = () => {
+  const dispatch = useAppDispatch();
+  const geoObjects = useAppSelector((state) => state.geoObjectsReducer);
+
+  const handleBtnClick = () => {
+    dispatch(setShow(false));
+    console.log(geoObjects.fullObjectInfo);
+  };
 
   return (
-    <SCard>
+    <>
+      <SBlock>
+        <SIconButtonArrow aria-label="show more info" onClick={handleBtnClick}>
+          <IoMdArrowDropleft />
+        </SIconButtonArrow>
+        <STitle>Избранное </STitle>
+      </SBlock>
+      <SCard>
         <SCardContent>
-            <SCardMedia
-                component="img"
-                image="https://masterpiecer-images.s3.yandex.net/c352b1b9801c11ee9607720ccb3e265f:upscaled"
-            />
-            <SCardHeader>Фантаcмагарический музей им. П.М. Машерова</SCardHeader>
-            <SText>Lörem ipsum jere. Intrabel peraktiv pävufåsk läslov pide. Exon prelogi. 
-                Någonstansare  begöpp. Homoadoption tesände keck såsom köttrymden. Epigen digon fast</SText>
+          <SCardMedia
+            component="img"
+            image="https://masterpiecer-images.s3.yandex.net/c352b1b9801c11ee9607720ccb3e265f:upscaled"
+          />
+          {geoObjects.fullObjectInfo.name && (
+            <SCardHeader>{geoObjects.fullObjectInfo.name}</SCardHeader>
+          )}
+          {geoObjects.fullObjectInfo.address && (
+            <SText>
+              <SName>Адрес: </SName>
+              {geoObjects.fullObjectInfo.address}
+            </SText>
+          )}
+          {geoObjects.fullObjectInfo.hours && (
+            <SText>
+              <SName>Время работы: </SName>
+              {geoObjects.fullObjectInfo.hours}
+            </SText>
+          )}
+          {geoObjects.fullObjectInfo.phone && (
+            <SText>
+              <SName>Телефон: </SName>
+              {geoObjects.fullObjectInfo.phone}
+            </SText>
+          )}
+          {geoObjects.fullObjectInfo.url && (
+            <>
+              <SName>Сайт: </SName>
+              <SAddress
+                href={geoObjects.fullObjectInfo.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {geoObjects.fullObjectInfo.url}
+              </SAddress>
+            </>
+          )}
         </SCardContent>
         <SCardActions>
-        <SButtonFav>
-          <IoMdBookmark /> Сохранено
-        </SButtonFav>
-        <SButtonGeo>
-          <FaLocationDot /> Маршрут
-        </SButtonGeo>
-      </SCardActions>
-    </SCard>
+          <SButtonFav>
+            <IoMdBookmark /> Сохранено
+          </SButtonFav>
+          <SButtonGeo>
+            <FaLocationDot /> Маршрут
+          </SButtonGeo>
+        </SCardActions>
+      </SCard>
+    </>
   );
-}
+};
 
 export default FullCard;
