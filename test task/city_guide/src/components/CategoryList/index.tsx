8967bd4f07bb1c168f97cd72886b62621f865433
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:48:25 +0300

    Update index.tsx

--- test task/city_guide/src/components/CategoryList/index.tsx
+++ test task/city_guide/src/components/CategoryList/index.tsx
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { categories } from '../../constants/categories';
-import {
-  SList, SLi, SAvatar, SBlock
-} from "./styled";
+import { SList, SLi, SAvatar, SBlock } from './styled';
 import { useAppDispatch } from '../../hooks/redux';
 import { setSelectedCategories } from '../../store/reducers/geoObjects';
 
 const CategoryList = () => {
-
   const [filterCategories, setFilterCategories] = useState([]);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-      setFilterCategories(categories);
+    setFilterCategories(categories);
   }, [categories]);
 
   const handleClick = (index: number) => {
     setFilterCategories(
-        filterCategories.map((el, i) =>
-            i === index ? { ...el, isSelected: !el.isSelected } : el
-        )
+      filterCategories.map((el, i) =>
+        i === index ? { ...el, isSelected: !el.isSelected } : el
+      )
     );
   };
 
   useEffect(() => {
-    dispatch(setSelectedCategories(filterCategories.filter(el => el.isSelected)));
-  }, [filterCategories])
+    dispatch(
+      setSelectedCategories(filterCategories.filter((el) => el.isSelected))
+    );
+  }, [filterCategories]);
 
   return (
     <SBlock>
       <SList>
         {filterCategories.map((category, index) => (
-          <SLi isSelected={category.isSelected} onClick={() => handleClick(index)}>
-            <SAvatar src={category.icon}/> {category.text}    
+          <SLi
+            isSelected={category.isSelected}
+            onClick={() => handleClick(index)}
+          >
+            <SAvatar src={category.icon} /> {category.text}
           </SLi>
         ))}
       </SList>
