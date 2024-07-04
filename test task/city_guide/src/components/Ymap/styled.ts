4a46bb1670ba3618f508b04d3a72456bd316c9f7
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 20:24:32 +0300

    Update styled.ts

--- test task/city_guide/src/components/Ymap/styled.ts
+++ test task/city_guide/src/components/Ymap/styled.ts
@@ -1,20 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SMap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: absolute;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
 `;
 
 export const SMdClose = styled.div`
-    position: absolute;
-    top: 10px;
-    background: #fff;
-    padding: 10px;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-    border: none;
-    right: ${ props => (props.isShow ? `10px` : `360px`)}; 
+  position: absolute;
+  top: 10px;
+  background: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  border: none;
+  right: ${(props) => (props.isShow ? `10px` : `360px`)};
 `;
-
