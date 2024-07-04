3690d03107b1517622f9e47b7c14a65702ab7099
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:41:03 +0300

    Update index.tsx

--- test task/city_guide/src/components/Card/index.tsx
+++ test task/city_guide/src/components/Card/index.tsx
@@ -1,40 +1,69 @@
 import React from 'react';
 import {
-    SCard, SCardMedia, SText, SDiv, SCardHeader, 
-    SIconButtonFav, SIconButtonArrow, SCardActions, SCardContent
-} from "./styled";
-import { IoMdArrowDropright, IoMdBookmark } from "react-icons/io";
+  SCard,
+  SCardMedia,
+  SText,
+  SDiv,
+  SCardHeader,
+  SIconButtonFav,
+  SIconButtonArrow,
+  SCardActions,
+  SCardContent,
+  SName,
+} from './styled';
+import { IoMdArrowDropright, IoMdBookmark } from 'react-icons/io';
 import { useAppDispatch } from '../../hooks/redux';
-import { setShow } from '../../store/reducers/geoObjects';
+import { setShow, setFullObjectInfo } from '../../store/reducers/geoObjects';
+import { FavoritesCollectionRef } from '../../firebase';
+import { query, where, deleteDoc, getDocs } from '@firebase/firestore';
 
-
-const Card = ({object}) => {
-
+const Card = ({ object }) => {
   const dispatch = useAppDispatch();
 
   const handleBtnClick = () => {
     dispatch(setShow(true));
+    dispatch(setFullObjectInfo(object));
+  };
+
+  const deleteFavorite = async () => {
+    const q = query(
+      FavoritesCollectionRef,
+      where('geoobject_id', '==', object.objectId)
+    );
+    const docSnap = await getDocs(q);
+    docSnap.forEach((doc) => {
+      deleteDoc(doc.ref);
+    });
   };
 
   return (
     <SCard>
       <SCardContent>
-            <SDiv>
-                <SCardMedia
-                    component="img"
-                    image="https://masterpiecer-images.s3.yandex.net/c352b1b9801c11ee9607720ccb3e265f:upscaled"
-                />
-                <SCardHeader>{object.name && (object.name)}</SCardHeader>
-            </SDiv>
-            {object.address && (
-              <SText>Адрес: {object.address}</SText>
-            )}
-            {object.hours && (
-              <SText>Время работы: {object.hours}</SText>
-            )}
+        <SDiv>
+          <SCardMedia
+            component="img"
+            image="https://masterpiecer-images.s3.yandex.net/c352b1b9801c11ee9607720ccb3e265f:upscaled"
+          />
+          {object.name && <SCardHeader>{object.name}</SCardHeader>}
+        </SDiv>
+        {object.address && (
+          <SText>
+            <SName>Адрес: </SName>
+            {object.address}
+          </SText>
+        )}
+        {object.hours && (
+          <SText>
+            <SName>Время работы: </SName>
+            {object.hours}
+          </SText>
+        )}
       </SCardContent>
       <SCardActions>
-        <SIconButtonFav aria-label="add to favorites">
+        <SIconButtonFav
+          aria-label="delete from favorites"
+          onClick={deleteFavorite}
+        >
           <IoMdBookmark />
         </SIconButtonFav>
         <SIconButtonArrow aria-label="show more info" onClick={handleBtnClick}>
@@ -43,6 +72,6 @@ const Card = ({object}) => {
       </SCardActions>
     </SCard>
   );
-}
+};
 
 export default Card;
