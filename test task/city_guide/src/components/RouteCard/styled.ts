42d43f3750c3f4be48e29dd110fd559b99b295c3
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:47:05 +0300

    Update styled.ts

--- test task/city_guide/src/components/RouteCard/styled.ts
+++ test task/city_guide/src/components/RouteCard/styled.ts
@@ -1,40 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SCard = styled.div`
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    width: 300px;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  width: 300px;
 `;
 
 export const SDiv = styled.div`
-    display: flex;
-    flex-direction: row;
-    color: #405F7B;
+  display: flex;
+  flex-direction: row;
+  color: #405f7b;
 `;
 
 export const SDistance = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-right: 40px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 40px;
 `;
 
 export const STime = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const STextValue = styled.div`
-    font-size: 20px;
-    font-weight: 700;
+  font-size: 20px;
+  font-weight: 700;
 `;
 
 export const STextName = styled.div`
-    font-size: 16px;
-    font-weight: 400;
-    opacity: 0.5;
+  font-size: 16px;
+  font-weight: 400;
+  opacity: 0.5;
 `;
