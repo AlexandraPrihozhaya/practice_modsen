489cabcb79dc8b4325ceacd88f0f610cf8cac4e4
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:59:48 +0300

    Update Home.tsx

--- test task/city_guide/src/pages/Home.tsx
+++ test task/city_guide/src/pages/Home.tsx
@@ -1,15 +1,14 @@
-import React from "react"
-import SideBarMenu from "../components/Sidebar"
-import MyMap from "../components/Ymap"
+import React from 'react';
+import SideBarMenu from '../components/Sidebar';
+import MyMap from '../components/Ymap';
 
 const Home = () => {
-
-    return (
-        <section style={{display: 'flex'}}>
-            <SideBarMenu /> 
-            <MyMap />
-        </section>
-    )
-}
+  return (
+    <section style={{ display: 'flex' }}>
+      <SideBarMenu />
+      <MyMap />
+    </section>
+  );
+};
 
 export default Home;
