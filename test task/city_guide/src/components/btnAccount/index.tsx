222a0bbd41d1ee95a8218a11217b84a2d41e2c7c
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 21:49:59 +0300

    Update index.tsx

--- test task/city_guide/src/components/btnAccount/index.tsx
+++ test task/city_guide/src/components/btnAccount/index.tsx
@@ -1,36 +1,50 @@
-import React from "react"
-import {
-  SAvatar, SButton
-} from "./styled";
+import React from 'react';
+import { SAvatar, SButton, SAvatarMobile } from './styled';
 import { Avatar } from '@mui/material';
-import { useAuth } from "../../hooks/useAuth";
-import { useAppDispatch } from "../../hooks/redux";
-import { removeUser } from "../../store/reducers/userSlice";
-import searchbtn from '@assets/searchbtn.png'
+import { useAuth } from '../../hooks/useAuth';
+import { useAppDispatch } from '../../hooks/redux';
+import { removeUser } from '../../store/reducers/userSlice';
+import searchbtn from '@assets/searchbtn.png';
+import { Desktop, Mobile } from '../../constants/adaptive';
 
 const BtnAccount = () => {
+  const { isAuth, email } = useAuth();
+  const dispatch = useAppDispatch();
 
-    const {isAuth} = useAuth();
-    const dispatch = useAppDispatch();
-
-    return (
-      isAuth ? (
-        <SAvatar>
-        <SButton
-          onClick={() => dispatch(removeUser())}>
-          <Avatar src="/broken-image.jpg"/>
-        </SButton>
-      </SAvatar>
-      ) : (
-        <SAvatar>
-          <a
-            href="/login"
-            role="button">
-            <Avatar src={searchbtn}/>
-          </a>
-        </SAvatar>
-      )
-    )
+  return (
+    <>
+      <Desktop>
+        {isAuth ? (
+          <SAvatar>
+            <SButton onClick={() => dispatch(removeUser())}>
+              <Avatar src="/broken-image.jpg" />
+            </SButton>
+          </SAvatar>
+        ) : (
+          <SAvatar>
+            <a href="/login" role="button">
+              <Avatar src={searchbtn} />
+            </a>
+          </SAvatar>
+        )}
+      </Desktop>
+      <Mobile>
+        {isAuth ? (
+          <SAvatarMobile>
+            <SButton onClick={() => dispatch(removeUser())}>
+              <Avatar src="/broken-image.jpg" />
+            </SButton>
+          </SAvatarMobile>
+        ) : (
+          <SAvatarMobile>
+            <a href="/login" role="button">
+              <Avatar src={searchbtn} />
+            </a>
+          </SAvatarMobile>
+        )}
+      </Mobile>
+    </>
+  );
 };
 
 export default BtnAccount;
