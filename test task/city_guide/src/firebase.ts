d56ddc67f4435cd66f63dadab337830d33ba68a8
Alexandra <90527945+AlexandraPrihozhaya@users.noreply.github.com>
2024-07-04 22:04:42 +0300

    Update firebase.ts

--- test task/city_guide/src/firebase.ts
+++ test task/city_guide/src/firebase.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "@firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAlLn3rTk6CFTgrBc37N2ENIdTrLIASqts",
-    authDomain: "app-mappie.firebaseapp.com",
-    projectId: "app-mappie",
-    storageBucket: "app-mappie.appspot.com",
-    messagingSenderId: "971752653190",
-    appId: "1:971752653190:web:f4009f9529041611c62c07"
+  apiKey: 'AIzaSyAlLn3rTk6CFTgrBc37N2ENIdTrLIASqts',
+  authDomain: 'app-mappie.firebaseapp.com',
+  projectId: 'app-mappie',
+  storageBucket: 'app-mappie.appspot.com',
+  messagingSenderId: '971752653190',
+  appId: '1:971752653190:web:f4009f9529041611c62c07',
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const FavoritesCollectionRef = collection(db, "favorites");
+export const FavoritesCollectionRef = collection(db, 'favorites');
